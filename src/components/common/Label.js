db435641b56c8a39684e5a07f6400d53c67b5893
Slavik Melnyk <yarikmelnyk39@gmail.com>
2023-02-09 16:16:11 +0200

    Delete src directory

--- src/components/common/Label.js
+++
@@ -1,9 +0,0 @@
-const Label = ({ data }) => (
-    <>
-        <span className='opacity-0'>{data.identifier}</span>
-            {data.label}
-        <span className='opacity-0'>{data.identifier}</span>
-    </>
-);
-
-export default Label;
